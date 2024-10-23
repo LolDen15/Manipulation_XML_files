993b321056a04ae19d68d32f6200c47dd9ca35ed
Денис Майоров <79066339008@yandex.ru>
2024-10-23 18:09:59 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import xml.etree.ElementTree as ET
+import sqlite3
+import re
+from datetime import datetime
+
+
+def is_valid_company(company):
+    pass
+
+
+def insert_or_update_company(company):
+    pass
+
+
+def insert_phones_company(company):
+    pass
+
+
+def parse_xml():
+    pass
+
+
+if __name__ == '__main__':
+    parse_xml()
